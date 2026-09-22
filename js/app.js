49d024294cf0cc1d93b23d54eb3428c986bc078a
aapjeapen <aapjeapen12345@gmail.com>
2026-09-23 01:41:18 +0200

    added darkmode button

---
+++ js/app.js
@@ -0,0 +1,21 @@
+/**
+ * voor darkbutton en verzorgt dat darkmodes blijft wanneer je van menu verandert
+ */
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+
+    try {
+        localStorage.setItem("darkmode", document.body.classList.contains("dark-mode") ? "aan" : "uit");
+    }
+    catch (fout) {
+    }
+}
+
+try {
+    if (localStorage.getItem("darkmode") === "aan") {
+        document.body.classList.add("dark-mode");
+    }
+}
+catch (fout) {
+}
